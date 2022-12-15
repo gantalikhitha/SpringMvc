cb0e4ca26bf6717d9646a64428753bdaf95ad09c
likhitha <gantalikhithareddy@gmail.com>
2022-12-15 17:58:24 +0530

    first commit

--- src/test/java/com/net/SpringBootThymeleafCrudExApplicationTests.java
+++ src/test/java/com/net/SpringBootThymeleafCrudExApplicationTests.java
@@ -20,24 +20,18 @@ import com.net.Service.StudentService;
 @SpringBootTest
 public class SpringBootThymeleafCrudExApplicationTests {
 
-	@Autowired
-	private StudentService studentService;
-
-	@MockBean
-	private StudentRepository studentRepository;
-
-	@Test
-	public void saveTest() {
-		Student student = new Student(1, "siva", "gpl");
-		when(studentRepository.save(student)).thenReturn(student);
-		// assertEquals(student, studentService.saveStudent(student));
-	}
-
-	@Test
-	public void deleteTest() {
-		Student student = new Student(5, "siva", "gpl");
-		studentService.deleteStudentById(student);
-		verify(studentRepository, times(1)).delete(student);
-	}
+	/*
+	 * @Autowired private StudentService studentService;
+	 * 
+	 * @MockBean private StudentRepository studentRepository;
+	 * 
+	 * @Test public void saveTest() { Student student = new Student(1, "siva",
+	 * "gpl"); when(studentRepository.save(student)).thenReturn(student); //
+	 * assertEquals(student, studentService.saveStudent(student)); }
+	 * 
+	 * @Test public void deleteTest() { Student student = new Student(5, "siva",
+	 * "gpl"); studentService.deleteStudentById(student); verify(studentRepository,
+	 * times(1)).delete(student); }
+	 */
 
 }
